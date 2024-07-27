32a43976d3f3f48478fdb63c91fd1ffc367ec432
jgbtsuchiya <joaog.tsuchiya@gmail.com>
2024-07-27 20:54:32 -0300

    Tratativa de erros no input

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ const form = document.getElementById('investment-form');
 
 function renderProgression(evento) {
     evento.preventDefault();
-    const startingAmount = Number(document.getElementById('starting-amount').value);
-    const additionalContribution = Number(document.getElementById('additional-contribution').value);
+
+    if (document.querySelector('.error')) {
+        return;
+    }
+    const startingAmount = Number(document.getElementById('starting-amount').value.replace(",", "."));
+    const additionalContribution = Number(document.getElementById('additional-contribution').value.replace(",", "."));
     const timeAmount = Number(document.getElementById('time-amount').value);
     const timeAmountPeriod = document.getElementById('time-amount-period').value;
-    const returnRate = Number(document.getElementById('return-rate').value);
+    const returnRate = Number(document.getElementById('return-rate').value.replace(",", "."));
     const returnRatePeriod = document.getElementById('evaluation-period').value;
-    const taxRate = Number(document.getElementById('tax-rate').value);
+    const taxRate = Number(document.getElementById('tax-rate').value.replace(",", "."));
 
     const returnsArray = generateReturnsArray(
         startingAmount,
@@ -22,6 +26,38 @@ function renderProgression(evento) {
     );
 
     console.log(returnsArray);
+}
+
+function validadeInput(evento) {
+    if (evento.target.value === '') {
+        return;
+    }
+
+    const { parentElement } = evento.target;
+    const grandParentElement = evento.target.parentElement.parentElement;
+    const inputValue = evento.target.value.replace(",", ".");
+
+    if (isNaN(inputValue)
+        || Number(inputValue) <= 0
+        && !parentElement.classList.contains("error")) {
+        const errorTextElement = document.createElement('p');
+        errorTextElement.classList.add('text-red-500');
+        errorTextElement.innerText = "Insira um valor número maior que zero";
+
+        parentElement.classList.add('error');
+        grandParentElement.appendChild(errorTextElement);
+    } else if (parentElement.classList.contains("error")
+        && !isNaN(inputValue)
+        && Number(inputValue) > 0) {
+        parentElement.classList.remove("error");
+        grandParentElement.querySelector('p').remove();
+    }
+}
+
+for (const formElement of form) {
+    if (formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+        formElement.addEventListener('blur', validadeInput);
+    }
 }
 
 form.addEventListener('submit', renderProgression);
