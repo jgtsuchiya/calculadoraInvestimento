ec06d2378bb7192f6d5f564d038cfef23b3931ab
jgbtsuchiya <joaog.tsuchiya@gmail.com>
2024-08-07 20:09:10 -0300

    Implantação do Carrosel

--- main.js
+++ main.js
@@ -17,24 +17,28 @@ const columnsArray = [
     },
     {
         columnLabel: "Total Investido", accessor: "investedAmount",
-        format: (numberInfo) => formatCurrency(numberInfo),
+        format: (numberInfo) => formatCurrencyToTable(numberInfo),
     },
     {
         columnLabel: "Rendimento Mensal", accessor: "interestReturns",
-        format: (numberInfo) => formatCurrency(numberInfo),
+        format: (numberInfo) => formatCurrencyToTable(numberInfo),
     },
     {
         columnLabel: "Rendimento Total", accessor: "totalInterestReturns",
-        format: (numberInfo) => formatCurrency(numberInfo),
+        format: (numberInfo) => formatCurrencyToTable(numberInfo),
     },
     {
         columnLabel: "Quantia Total", accessor: "totalAmount",
-        format: (numberInfo) => formatCurrency(numberInfo),
+        format: (numberInfo) => formatCurrencyToTable(numberInfo),
     },
 ];
 
-function formatCurrency(value) {
+function formatCurrencyToTable(value) {
     return value.toLocaleString('pt-br', { style: "currency", currency: "BRL" });
+}
+
+function formatCurrencyToGraph(value) {
+    return value.toFixed(2);
 }
 
 function renderProgression(evento) {
@@ -65,65 +69,63 @@ function renderProgression(evento) {
 
     const finalInvestmentObject = returnsArray[returnsArray.length - 1];
 
+    doughnutChartReference = new Chart(finalMoneyChart, {
+        type: 'doughnut',
+        data: {
+            labels: [
+                'Total Investido',
+                'Rendimento',
+                'Imposto'
+            ],
+            datasets: [
+                {
+                    data: [
+                        formatCurrencyToGraph(finalInvestmentObject.investedAmount),
+                        formatCurrencyToGraph(finalInvestmentObject.totalInterestReturns * (1 - (taxRate / 100))),
+                        formatCurrencyToGraph(finalInvestmentObject.totalInterestReturns * (taxRate / 100))
+                    ],
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                }
+            ]
+        },
+    });
 
-    /*
-        doughnutChartReference = new Chart(finalMoneyChart, {
-            type: 'doughnut',
-            data: {
-                labels: [
-                    'Total Investido',
-                    'Rendimento',
-                    'Imposto'
-                ],
-                datasets: [
-                    {
-                        data: [
-                            formatCurrency(finalInvestmentObject.investedAmount),
-                            formatCurrency(finalInvestmentObject.totalInterestReturns * (1 - (taxRate / 100))),
-                            formatCurrency(finalInvestmentObject.totalInterestReturns * (taxRate / 100))
-                        ],
-                        backgroundColor: [
-                            'rgb(255, 99, 132)',
-                            'rgb(54, 162, 235)',
-                            'rgb(255, 205, 86)'
-                        ],
-                        hoverOffset: 4
-                    }
-                ]
-            },
-        });
-    
-        progressionChartReference = new Chart(progressionChart, {
-            type: 'bar',
-            data: {
-                labels: returnsArray.map(investmentObject => investmentObject.month),
-                datasets: [
-                    {
-                        label: 'Total Investido',
-                        data: returnsArray.map((investmentObject) =>
-                            formatCurrency(investmentObject.investedAmount)),
-                        backgroundColor: 'rgb(255, 99, 132)',
-                    },
-                    {
-                        label: 'Retorno do Investimento',
-                        data: returnsArray.map((investmentObject) =>
-                            formatCurrency(investmentObject.interestReturns)),
-                        backgroundColor: 'rgb(54, 162, 235)',
-                    }
-                ]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    x: {
-                        stacked: true,
-                    },
-                    y: {
-                        stacked: true,
-                    }
+    progressionChartReference = new Chart(progressionChart, {
+        type: 'bar',
+        data: {
+            labels: returnsArray.map(investmentObject => investmentObject.month),
+            datasets: [
+                {
+                    label: 'Total Investido',
+                    data: returnsArray.map((investmentObject) =>
+                        formatCurrencyToGraph(investmentObject.investedAmount)),
+                    backgroundColor: 'rgb(255, 99, 132)',
+                },
+                {
+                    label: 'Retorno do Investimento',
+                    data: returnsArray.map((investmentObject) =>
+                        formatCurrencyToGraph(investmentObject.interestReturns)),
+                    backgroundColor: 'rgb(54, 162, 235)',
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    stacked: true,
+                },
+                y: {
+                    stacked: true,
                 }
             }
-        })*/
+        }
+    })
 
     createTable(columnsArray, returnsArray, 'results-table');
 }
@@ -186,6 +188,19 @@ for (const formElement of form) {
         formElement.addEventListener('blur', validadeInput);
     }
 }
+
+const mainEl = document.querySelector('main');
+const carouselEl = document.getElementById('carousel');
+const nextButton = document.getElementById('slide-arrow-next');
+const previousBUtton = document.getElementById('slide-arrow-previous');
+
+nextButton.addEventListener('click', () => {
+    carouselEl.scrollLeft += mainEl.clientWidth;
+})
+
+previousBUtton.addEventListener('click', () => {
+    carouselEl.scrollLeft -= mainEl.clientWidth;
+})
 
 form.addEventListener('submit', renderProgression);
 clearFormButton.addEventListener('click', clearForm)
